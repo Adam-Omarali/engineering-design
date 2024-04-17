a5172942bb0ed31674dd1a23b548e5f7585dffd8
Adam <adamomarali37@gmail.com>
2024-04-17 19:30:06 -0400

    updated images and trying to get rid of nextra

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
-import { useRouter } from "next/router";
-import { useConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>Adam's Engineering Handbook</span>,
@@ -23,23 +21,11 @@ const config: DocsThemeConfig = {
     defaultTheme: "light",
   },
   head: () => {
-    const { asPath, defaultLocale, locale } = useRouter();
-    const { frontMatter } = useConfig();
-    const url =
-      "https://adamsengineeringhandbook.vercel.app" +
-      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
-
     return (
       <>
-        <meta property="og:url" content={url} />
-        <meta
-          property="og:title"
-          content={frontMatter.title || "Engineering Handbook"}
-        />
-        <meta
-          property="og:description"
-          content={frontMatter.description || "An engineering handbook"}
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content={"Engineering Handbook"} />
+        <meta property="og:description" content={"An engineering handbook"} />
       </>
     );
   },
