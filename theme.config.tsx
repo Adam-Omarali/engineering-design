ea4eab259fa7c578d169c24fb977a323e3d21b7c
Adam <adamomarali37@gmail.com>
2024-04-17 19:21:52 -0400

    seo stuff

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
+import { useConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>Adam's Engineering Handbook</span>,
@@ -19,6 +21,27 @@ const config: DocsThemeConfig = {
   },
   nextThemes: {
     defaultTheme: "light",
+  },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+    const url =
+      "https://adamsengineeringhandbook.vercel.app" +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta
+          property="og:title"
+          content={frontMatter.title || "Engineering Handbook"}
+        />
+        <meta
+          property="og:description"
+          content={frontMatter.description || "An engineering handbook"}
+        />
+      </>
+    );
   },
 };
 
